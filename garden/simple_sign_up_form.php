b36036e0a59e4a3f637ab453b073efb14c5a20ff
Adetoyebamise <55400020+Adetoyebamise@users.noreply.github.com>
2020-08-11 17:03:41 -0700

    mi o lemi intoto for now am still learning

---
+++ garden/simple_sign_up_form.php
@@ -0,0 +1,29 @@
+<?php
+$firstname = $_POST['firstname'];
+$secondname = $_POST['secondname'];
+$email = $_POST['email'];
+$date_of_birth = $_POST['date_of_birth'];
+$gender_type = $_POST['gender_type'];
+$department = $_POST['department'];
+$password = $_POST['password'];
+
+echo 'You logged in as'. $_username $_secondname '<br/>' ;
+echo we can reach you through your email "$_email"<br/>;
+echo Remember to celebrate you Birthday "$_Date_of_Birth";
+	if (){age 25, age++  2000<age<1990 
+	echo you are in your twenties;}
+	  if () { //If there is an input of female
+	  echo your Gender type is Male};
+	 else {echo your gender type is Female};
+	  if (choosecolor){//If user pick any specific color
+	  style=background-color}
+	 if (){//
+		 echo are in the "IT" Department;
+	 }elseif{//if there is a input of HR
+		 echo You are in the "HR" Department;
+	 }
+	 else{//if there is a input of stuff
+		 echo You are in the "stuff" Department;
+	 }
+ 
+?>
